317c399ae0527c886a6cd65bb69c9b3000aa032c
Dan Pitic <dpitic@gmail.com>
2017-04-09 17:00:40 +0800

    Completed signal handler and Chapter 1. Up next: Chapter 2.

---
+++ ch01/uidgid.c
@@ -0,0 +1,10 @@
+/*
+ * This program outputs the user's ID (uid) and group ID (gid) to stdout.
+ */
+#include "apue.h"
+
+int main(void)
+{
+	printf("uid = %d, gid = %d\n", getuid(), getgid());
+	exit(0);
+}
