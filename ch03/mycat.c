a4a553e08ab9948e5b2a5829ae36f5d3e021e320
Dan Pitic <dpitic@gmail.com>
2017-04-17 10:11:45 +0800

    Reformat code style.

--- ch03/mycat.c
+++ ch03/mycat.c
@@ -16,14 +16,12 @@ int main(void)
 	char buf[BUFFSIZE];		/* read and write buffer */
 
 	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) {
-		if (write(STDOUT_FILENO, buf, n) != n)
-		{
+		if (write(STDOUT_FILENO, buf, n) != n) {
 			err_sys("write error");
 		}
 	}
 
-	if (n < 0)
-	{
+	if (n < 0) {
 		err_sys("read error");
 	}
 
