e93246face2ea4f019ffe0e789cd7e84e45ef887
Dan Pitic <dpitic@gmail.com>
2017-04-22 21:54:57 +0800

    Completed unlink.

--- ch04/unlink.c
+++ ch04/unlink.c
@@ -1,3 +1,19 @@
 /*
- * 
+ * This program opens a file and then unlinks it. The program then goes to
+ * sleep for 15 seconds before terminating.
  */
+#include "apue.h"
+#include <fcntl.h>
+
+int main(void) {
+  if (open("tempfile", O_RDWR) < 0) {
+    err_sys("open error");
+  }
+  if (unlink("tempfile") < 0) {
+    err_sys("unlink error");
+  }
+  printf("file unlinked\n");
+  sleep(15);
+  printf("done\n");
+  exit(0);
+}
