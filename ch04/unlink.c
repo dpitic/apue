8c3e1daec9b2573120afd1e82687098d4d0c0c1a
Dan Pitic <dpitic@gmail.com>
2017-04-23 10:26:48 +0800

    Added debug code to unlink to test on Mac.

--- ch04/unlink.c
+++ ch04/unlink.c
@@ -6,8 +6,13 @@
 #include <fcntl.h>
 
 int main(void) {
-  if (open("tempfile", O_RDWR) < 0) {
+  int fd;
+  if ((fd = open("tempfile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)) < 0) {
     err_sys("open error");
+  }
+  printf("tempfile fd = %d\n", fd);
+  if (write(fd, "hello", 5) != 5) {
+    err_sys("write error");
   }
   if (unlink("tempfile") < 0) {
     err_sys("unlink error");
