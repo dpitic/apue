48f3b6c8bec3c2c937a8347a9864cbe39942ce48
Dan Pitic <dpitic@gmail.com>
2017-04-23 12:16:50 +0800

    Further testing of unlink to confirm correct operation. Addition of comments in source file to explain differences with implementation in the book.

--- ch04/unlink.c
+++ ch04/unlink.c
@@ -1,14 +1,29 @@
 /*
  * This program opens a file and then unlinks it. The program then goes to
  * sleep for 15 seconds before terminating.
+ *
+ * The behavour of this program is different to the example behaviour shown in
+ * the book. The book shows that the tempfile is created and shows in the
+ * directory listing. However, by immediately unlinking the file, the temporary
+ * file does not show as a file listing in the directory.
  */
 #include "apue.h"
 #include <fcntl.h>
 
 int main(void) {
+  /* 
+   * Extra flags (and mode) are required to create the new tempfile, otherwise
+   * the file is not created. The implementation in the book lacks these flags
+   * and consequently, the tempfile is not created. 
+   */
   if (open("tempfile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR) < 0) {
     err_sys("open error");
   }
+  /*
+   * Immediately unlinking the file prevents the file from showing in the
+   * directory listing. However, the file is available for reading and writing
+   * until the program terminates.
+   */
   if (unlink("tempfile") < 0) {
     err_sys("unlink error");
   }
