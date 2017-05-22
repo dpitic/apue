4dfacd5df7df8f4ec48ec7f6e69a841e1e1c807a
Dan Pitic <dpitic@gmail.com>
2017-05-22 21:15:05 +0800

    Completed exit example. Up next: atexit() function.

---
+++ ch07/hello1.c
@@ -0,0 +1,9 @@
+/*
+ * This program is the classic "hello, world" example, used here to demonstrate
+ * various exit behaviour when using different versions of the C standard.
+ */
+#include <stdio.h>
+
+main() { 
+    printf("hello, world\n");
+}
