e9e37aeb789088a9c35f42667cf0de5686fc696b
Dan Pitic <dpitic@gmail.com>
2017-05-28 11:17:00 +0800

    Completed setjmp & longjmp examples. Up next: Automatic, Register & Volatile Variables.

---
+++ ch07/cmd2..c
@@ -0,0 +1,67 @@
+/*
+ * Program skeleton for command processing.  Consists of a main loop that reads
+ * lines from stdin and calls do_line() to process each line.  This function
+ * then calls get_token() to fetch the next token from the input line.  The
+ * first token of a line is assumed to be a command of some form, and a switch
+ * statement selects each command.  For the single command shown, the function
+ * cmd_add() is called.  This is a skeleton for a typical program that reads
+ * commands, determines the command type, and then calls functions to process
+ * each command.
+ *
+ * This program extends cmd1 to demonstrate the use of setjmp and longjmp.
+ */
+#include "apue.h"
+#include <setjmp.h>
+
+#define TOK_ADD 5
+
+jmp_buf jmpbuffer;
+
+void do_line(char *);
+void cmd_add(void);
+int get_token(void);
+
+int main(void) {
+  char line[MAXLINE];
+
+  /* call setjmp from the location that we want to return to */
+  if (setjmp(jmpbuffer) != 0) { /* called directly; expect return value = 0 */
+    printf("error");
+  }
+  /* main loop reads lines from stdin & calls do_line() to process each line */
+  while (fgets(line, MAXLINE, stdin) != NULL) {
+    do_line(line);
+  }
+  exit(0);
+}
+
+char *tok_ptr; /* global pointer for get_token() */
+
+/* Process one line of input */
+void do_line(char *ptr) {
+  int cmd;
+
+  tok_ptr = ptr;
+  while ((cmd = get_token()) > 0) {
+    switch (cmd) { /* one case for each command */
+    case TOK_ADD:
+      cmd_add();
+      break;
+    }
+  }
+}
+
+void cmd_add(void) {
+  int token;
+
+  token = get_token();
+  if (token < 0) { /* an error has occurred */
+    /* second argument is non-zero return value from setjmp() in main() */
+    longjmp(jmpbuffer, 1);
+  }
+  /* rest of processing for this command */
+}
+
+int get_token(void) {
+  /* fetch next token from line pointed to by tok_ptr */
+}
