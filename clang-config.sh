70ed7a347a203c7520294879014ec034f49aff8c
Dan Pitic <dpitic@gmail.com>
2018-01-29 09:02:32 +0800

    Created new shell script to configure .clang_complete file and associated templates for platforms.

---
+++ clang-config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# 
+# File:   clang-config.sh
+# Author: dpitic
+#
+# Created on 29/01/2018, 8:44:05 AM
+#
+# Configures the .clang_complete file based on system type.
+#
+
+case `./systype.sh` in
+"freebsd")
+    cp .clang_complete.bsd .clang_complete
+    ;;
+"openbsd")
+    cp .clang_complete.bsd .clang_complete
+    ;;
+"linux")
+    cp .clang_complete.linux .clang_complete
+    ;;
+"macos")
+    cp .clang_complete.macos .clang_complete
+    ;;
+*)
+    echo "Unknown platform" >&2
+    exit 1
+esac
